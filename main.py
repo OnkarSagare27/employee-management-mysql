4cb3253ad3722a8b5f87fb7729b91045deaf71f5
OnkarSagare27 <onkardsagare@gmail.com>
2023-11-01 04:11:10 +0530

    Configured buttons on home screen

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import tkinter as tk
+from tkinter import *
 from sqlClient import mySqlClient
 import json
 
@@ -7,16 +7,36 @@ configData = json.load(file)
 
 sqlClient = mySqlClient(username=configData['user'], password=configData['pass'], host=configData['host'], database=configData['database'])
 
-window = tk.Tk()
+window = Tk()
 
 screen_width = window.winfo_screenwidth()
 screen_height = window.winfo_screenheight()
 
-window_width = int(screen_width * 0.5)
-window_height = int(screen_height * 0.5)
+window_width = int(screen_width * 0.7)
+window_height = int(screen_height * 0.7)
 
 window.geometry(f"{window_width}x{window_height}")
 
 window.title("Employee Management System")
+
+def clearScreen():
+    for widget in window.winfo_children():
+        widget.destroy()
+
+button_labels = ["Add new employee","Delete an employee", "Update an employee", "Get details of an employee", "View all employees"]
+
+frame = Frame(window, bg='grey')
+addButton = Button(frame, text='Add New Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
+addButton.pack(padx=10, pady=10)
+addButton = Button(frame,text='Delete An Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
+addButton.pack(padx=10, pady=10)
+addButton = Button(frame, text='Update An Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
+addButton.pack(padx=10, pady=10)
+addButton = Button(frame,text='Get Details of An\nEmployee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
+addButton.pack(padx=10, pady=10)
+addButton = Button(frame,text='View All Employees',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
+addButton.pack(padx=10, pady=10)
+frame.pack()
+frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 window.mainloop()
