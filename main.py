e78226f7ecefda9fd03f77a07138755fe18becdd
OnkarSagare27 <onkardsagare@gmail.com>
2023-11-01 10:21:40 +0530

    Added delete screen

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def homeScreen():
     opetionsFrame.grid(row= 0, column=0, padx=20, pady=10)
     addButton = Button(opetionsFrame, text='Add New Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal', command=addScreen)
     addButton.pack(padx=10, pady=10)
-    addButton = Button(opetionsFrame,text='Delete An Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
+    addButton = Button(opetionsFrame,text='Delete An Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal', command=deleteScreen)
     addButton.pack(padx=10, pady=10)
     addButton = Button(opetionsFrame, text='Update An Employee',height=2, width=15, padx=20, pady=20, font='lucida 10 normal')
     addButton.pack(padx=10, pady=10)
@@ -62,38 +62,88 @@ def addScreen():
     frame = Frame(window)
     frame.pack()
 
-    userInfoFrame =LabelFrame(frame, text="Employee Information")
-    userInfoFrame.grid(row= 0, column=0, padx=20, pady=10)
+    employeeDetailsFrame =LabelFrame(frame, text="Add An Employee")
+    employeeDetailsFrame.grid(row= 0, column=0, padx=20, pady=10)
 
-    firstNameLabel = Label(userInfoFrame, text="First Name")
+    firstNameLabel = Label(employeeDetailsFrame, text="First Name")
     firstNameLabel.grid(row=0, column=0)
-    lastNameLabel = Label(userInfoFrame, text="Last Name")
+    lastNameLabel = Label(employeeDetailsFrame, text="Last Name")
     lastNameLabel.grid(row=0, column=1)
-    birthDateLabel = Label(userInfoFrame, text="Birth Date (yyyy-mm-dd)")
+    birthDateLabel = Label(employeeDetailsFrame, text="Birth Date (yyyy-mm-dd)")
     birthDateLabel.grid(row=0, column=2)
-    joiningDateLabel = Label(userInfoFrame, text="Joining Date (yyyy-mm-dd)")
+    joiningDateLabel = Label(employeeDetailsFrame, text="Joining Date (yyyy-mm-dd)")
     joiningDateLabel.grid(row=2, column=0)
-    salaryLabel = Label(userInfoFrame, text="Salary")
+    salaryLabel = Label(employeeDetailsFrame, text="Salary")
     salaryLabel.grid(row=2, column=1)
 
-    firstNameEntry = Entry(userInfoFrame)
-    lastNameEntry = Entry(userInfoFrame)
-    birthDateEntry = Entry(userInfoFrame)
-    joiningDateEntry = Entry(userInfoFrame)
-    salaryEntry = Entry(userInfoFrame)
+    firstNameEntry = Entry(employeeDetailsFrame)
+    lastNameEntry = Entry(employeeDetailsFrame)
+    birthDateEntry = Entry(employeeDetailsFrame)
+    joiningDateEntry = Entry(employeeDetailsFrame)
+    salaryEntry = Entry(employeeDetailsFrame)
     firstNameEntry.grid(row=1, column=0)
     lastNameEntry.grid(row=1, column=1)
     birthDateEntry.grid(row=1, column=2)
     joiningDateEntry.grid(row=3, column=0)
     salaryEntry.grid(row=3, column=1)
 
-    for widget in userInfoFrame.winfo_children():
+    for widget in employeeDetailsFrame.winfo_children():
         widget.grid_configure(padx=10, pady=5)
 
     button = Button(frame, text="Save Employee Details", command= enterData)
     button.grid(row=3, column=0, sticky="news", padx=20, pady=10)
     button = Button(frame, text="Cancel", command= homeScreen)
     button.grid(row=4, column=0, sticky="news", padx=20, pady=10)
+    frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+def deleteScreen():
+    for widget in window.winfo_children():
+        widget.destroy()
+    frame = Frame(window)
+    frame.pack()
+
+    findEmployeeFrame =LabelFrame(frame, text="Delete An Employee")
+    findEmployeeFrame.grid(row= 0, column=0, padx=20, pady=10)
+    options = [
+    "Id",
+    "Name",
+    "Birth Date",
+    "Joining Date",
+    "Salary",
+    ]
+    usingOpt = StringVar(findEmployeeFrame)
+    usingOpt.set("Find Employee Using")
+
+    menu = OptionMenu(findEmployeeFrame, usingOpt, *options)
+    menu.grid(row=0, column=0,padx=10, pady=10)
+
+    def nextDelete():
+        usingOptValue = usingOpt.get()
+        if usingOptValue == 'Find Employee Using':
+            messagebox.showwarning(title="Error", message="Select an option first.")
+        else:
+            for widget in window.winfo_children():
+                widget.destroy()
+            frame = Frame(window)
+            frame.pack()
+
+            nextDeleteFrame =LabelFrame(frame, text="Delete An Employee")
+            nextDeleteFrame.grid(row= 0, column=0, padx=20, pady=10)
+
+            valueLable = Label(nextDeleteFrame, text=f"Enter {usingOptValue}")
+            valueLable.grid(row=0, column=0)
+            valueEntry = Entry(nextDeleteFrame, width=40)
+            valueEntry.grid(row=1, column=0,padx=10, pady=10)
+            button = Button(nextDeleteFrame, text="Find", command=nextDelete)
+            button.grid(row=2, column=0,sticky="news",padx=10, pady=10)
+            button = Button(nextDeleteFrame ,text="Back", command=deleteScreen)
+            button.grid(row=3, column=0,sticky="news",padx=10, pady=10)
+            frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+
+    button = Button(findEmployeeFrame, text="Next", command=nextDelete, width=25)
+    button.grid(row=1, column=0,sticky="news",padx=10, pady=10)
+    button = Button(findEmployeeFrame ,text="Cancel", command=homeScreen)
+    button.grid(row=2, column=0,sticky="news",padx=10, pady=10)
     frame.place(relx=0.5, rely=0.5, anchor=CENTER)
 
 homeScreen()
--- sqlClient.py
+++ sqlClient.py
@@ -22,3 +22,30 @@ class mySqlClient:
         insertQuery = "INSERT INTO employeedetails (name, dateOfBirth, joiningDate, salary) VALUES (%s, %s, %s, %s)"
         self.cursor.execute(insertQuery, (name, dateOfBirth, joiningDate, salary))
         self.sqlClient.commit()
+
+    def findEmployee(self, method: str, value: str):
+        '''
+        method: Id/Name/Birth Date/Joining Date/Salary
+        value: Value to find
+        '''
+        idQuery = "SELECT * FROM employeedetails WHERE id = %s"
+        nameQuery = "SELECT * FROM employee_details WHERE name LIKE %s"
+        joiningDateQuery = "SELECT * FROM employeedetails WHERE joiningDate = %s"
+        birthDateQuery = "SELECT * FROM employeedetails WHERE dateOfBirth = %s"
+        salaryQuery = "SELECT * FROM employeedetails WHERE salary = %s"
+        
+        if method == 'Id':
+            self.cursor.execute(idQuery, int(value))
+            return self.cursor.fetchall()
+        elif method == 'Name':
+            self.cursor.execute(nameQuery, ('%' + value + '%',))
+            return self.cursor.fetchall()
+        elif method == 'Birth Date':
+            self.cursor.execute(birthDateQuery, value)
+            return self.cursor.fetchall()
+        elif method == 'Joining Date':
+            self.cursor.execute(joiningDateQuery, value)
+            return self.cursor.fetchall()
+        else:
+            self.cursor.execute(salaryQuery, value)
+            return self.cursor.fetchall()
